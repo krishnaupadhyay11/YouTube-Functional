0335bc50042562d5563ca8438c59e48de942d544
Krishna Upadhyay <krishna.upadhyay911@gmail.com>
2024-03-07 22:36:09 +0530

    Keepgo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Stack } from "@mui/material"
+import { Link } from "react-router-dom"
+
+import {logo} from '../utils/Constants'
+import SearchBar from './SearchBar'
+
 export default function Navbar(){
     return (
-        <>
-            SeetaRam
-        </>
+        <Stack
+        direction="row"
+        alignItems="center"
+        p={2}
+        sx={{position: 'sticky', backgroundColor: '#000', justifyContent: 'space-between'}}>
+            <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
+                <img src={logo} alt="logo" height={45} />
+            </Link>
+            <SearchBar />
+        </Stack>
     )
 }
